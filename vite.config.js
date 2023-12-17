83b11775b7dc743a108f4b7b133379559649ff8e
Weining Mai <84426364+weibb123@users.noreply.github.com>
2023-12-17 11:51:19 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/personal_website",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
   

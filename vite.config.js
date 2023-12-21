9fa566b375015c1b6b0d6234b69f9d9226f194d2
Weining Mai <84426364+weibb123@users.noreply.github.com>
2023-12-20 20:46:15 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  },
+  
   base: "/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
